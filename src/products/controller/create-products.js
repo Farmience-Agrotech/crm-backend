8e14b3fe4081a4987d350a040eb3fc627f6d3017
harsha vardhan <108379858+harshavardhanSDE@users.noreply.github.com>
2025-12-27 11:17:32 +0530

    fix: create product

--- src/products/controller/create-products.js
+++ src/products/controller/create-products.js
@@ -13,29 +13,38 @@ exports.createProduct = async ( req, res) => {
             return res.status(400).json({message: "Product already exists."});
         }
 
-        const template = await Templates.findOne({ _id: templateId});
 
-        if ( !template ) {
-            return res.status(400).json({message: "Template not found"});
+        let finalAdditionalFields = additionalFields || [];
+
+        if ( templateId ) {
+            const template = await Templates?.findOne({ _id: templateId});
+
+            if ( !template ) {
+                return res.status(400).json({message: "Template not found"});
+            }
+
+            const fieldsFromTemplate = template.templateFields.map(
+                field => {
+                    return {
+                        fieldName: field.fieldName,
+                        fieldType: field.fieldType,
+                        value: templateValues[field.fieldName] || "",
+                    }
+                }
+            );
+
+            finalAdditionalFields = [
+                ...fieldsFromTemplate,
+                ...(additionalFields || [])
+            ]
         }
 
-        const fieldsFromTemplate = template.templateFields.map(
-            field => {
-                return {
-                    fieldName: field.fieldName,
-                    fieldType: field.fieldType,
-                    value: templateValues[field.fieldName] || "",
-                }
-            }
-        );
 
-        const finalAdditionalFields = [
-            ...fieldsFromTemplate,
-            ...(additionalFields || [])
-        ]
+
+
         const product = await Products.create({
             name, description, sku, unit, categories, minPrice, maxPrice, taxRate, inventoryLocation,
-            stockQuantity, minStockLevel, minOrderLevel, additionalFields: finalAdditionalFields
+            stockQuantity, minStockLevel, minOrderLevel, additionalFields: finalAdditionalFields, templateId: templateId || null
         });
 
         res.status(201).json(product);
